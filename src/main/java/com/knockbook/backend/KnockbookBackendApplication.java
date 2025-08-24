affd09a8e77b8d4d26ab60c00d00ed418e8566a5
hwapyeonglee <60067186+hwapyeonglee@users.noreply.github.com>
2025-08-25 01:05:20 +0900

    init project (#2)

---
+++ src/main/java/com/knockbook/backend/KnockbookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.knockbook.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnockbookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnockbookBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knockbook/backend/KnockbookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knockbook.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnockbookBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
