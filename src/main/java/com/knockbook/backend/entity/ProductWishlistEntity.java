d33be9865ed209c23f556ab8438d1240a82b661f
suhyeon <rainbowjane@naver.com>
2025-09-09 17:15:42 +0900

    Create ProductWishlistEntity

---
+++ src/main/java/com/knockbook/backend/entity/ProductWishlistEntity.java
@@ -0,0 +1,29 @@
+package com.knockbook.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "products_wishlist")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class ProductWishlistEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "wishlist_id", nullable = false)
+    private Long wishlistId;
+
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Builder.Default
+    @Column(name = "is_wished", nullable = false)
+    private Boolean isWished = true; // 토글: true=찜, false=해제
+}
+
