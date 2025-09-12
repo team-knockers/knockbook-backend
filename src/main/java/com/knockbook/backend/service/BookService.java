9c9ee41ab685963ad106244083decebe9d9f5169
hwapyeonglee <hwapyeong.lee@gmail.com>
2025-09-12 15:20:39 +0900

    Remove unused imports in BookService

--- src/main/java/com/knockbook/backend/service/BookService.java
+++ src/main/java/com/knockbook/backend/service/BookService.java
@@ -1,10 +1,7 @@
 package com.knockbook.backend.service;
 
 import com.knockbook.backend.domain.Book;
-import com.knockbook.backend.repository.BookCategoryRepository;
 import com.knockbook.backend.repository.BookRepository;
-import com.knockbook.backend.repository.BookSubcategoryRepository;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,18 +13,12 @@ public class BookService {
     @Autowired
     private BookRepository bookRepository;
 
-    @Autowired
-    private BookCategoryRepository categoryRepository;
-
-    @Autowired
-    private BookSubcategoryRepository subcategoryRepository;
-
     public Page<Book> getBooksByCategory(
             String categoryCodeName, String subcategoryCodeName, Pageable pageable,
             String sortBy, String order, Integer maxPrice, Integer minPrice) {
 
         // BookRepository에 ID 전달 → 페이징 결과(Page<Book>) 반환
-        return bookRepository.findByCategory(categoryCodeName, subcategoryCodeName, pageable, sortBy, order, maxPrice, minPrice);
+        return bookRepository.findByCategory(categoryCodeName, subcategoryCodeName, pageable,
+                sortBy, order, maxPrice, minPrice);
     }
-
 }
