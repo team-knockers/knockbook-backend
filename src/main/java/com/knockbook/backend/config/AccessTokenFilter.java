ef6de4779cac7315c014fbcbdaa44a57e0500228
hwapyeonglee <hwapyeong.lee@gmail.com>
2025-09-14 15:04:06 +0900

    chore

--- src/main/java/com/knockbook/backend/config/AccessTokenFilter.java
+++ src/main/java/com/knockbook/backend/config/AccessTokenFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -19,7 +20,8 @@ import java.util.List;
 @Component
 public class AccessTokenFilter extends OncePerRequestFilter {
 
-    private final JWTComponent jwtComponent;
+    @Autowired
+    private JWTComponent jwtComponent;
 
     public AccessTokenFilter(JWTComponent jwtComponent) {
         this.jwtComponent = jwtComponent;
