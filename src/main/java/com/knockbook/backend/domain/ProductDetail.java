196dcffb9429435b6293af04ec60650f41bac2df
suhyeon <rainbowjane@naver.com>
2025-09-15 11:56:39 +0900

    Create ProductDetail

--- src/main/java/com/knockbook/backend/domain/ProductDetail.java
+++ src/main/java/com/knockbook/backend/domain/ProductDetail.java
@@ -1,4 +1,36 @@
 package com.knockbook.backend.domain;
 
+import jdk.jshell.Snippet;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class ProductDetail {
+    public enum Status { ACTIVE, HIDDEN, DISCONTINUED }
+    public enum Availability { AVAILABLE, OUT_OF_STOCK, PREORDER, BACKORDER, COMING_SOON, SOLD_OUT }
+
+    private Long id;
+    private Long categoryId;
+    private String sku;
+    private String name;
+    private Integer unitPriceAmount;
+    private Integer salePriceAmount;
+    private String manufacturerName;
+    private String isImported;
+    private String importCountry;
+    private Integer stockQty;
+    private Status status;
+    private Availability availability;
+    private BigDecimal averageRating;
+    private Integer reviewCount;
+
+    private List<String> galleryImageUrls;
+    private List<String> descriptionImageUrls;
 }
