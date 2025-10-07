322804f8e170299967da79c3340e21db5b470b05
suhyeon <rainbowjane@naver.com>
2025-10-08 00:23:45 +0900

    add review domain models: ProductReview, ProductReviewStats, ProductReviewsResult

--- src/main/java/com/knockbook/backend/domain/ProductReviewStats.java
+++ src/main/java/com/knockbook/backend/domain/ProductReviewStats.java
@@ -1,4 +1,17 @@
 package com.knockbook.backend.domain;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class ProductReviewStats {
+    private double averageRating;
+    private Map<Integer, Integer> starCounts;
+    private Long totalItems;
 }
