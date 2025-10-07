d2bbed7040fbe02e03d95747b45e5dc7caa4dab6
suhyeon <rainbowjane@naver.com>
2025-10-08 01:16:40 +0900

    add ProductReviewDTO and GetProductReviewsResponse

--- src/main/java/com/knockbook/backend/dto/GetProductReviewsResponse.java
+++ src/main/java/com/knockbook/backend/dto/GetProductReviewsResponse.java
@@ -1,4 +1,23 @@
 package com.knockbook.backend.dto;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class GetProductReviewsResponse {
+    private List<ProductReviewDTO> productReviews;
+
+    private Integer page;
+    private Integer size;
+    private Long totalItems;
+    private Integer totalPages;
+
+    private Double averageRating;
+    private Map<Integer, Integer> starCounts;
 }
