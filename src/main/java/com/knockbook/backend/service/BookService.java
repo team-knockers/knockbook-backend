7836796a2f17b2f623fdd9948a283cd2fb817560
Yoonhwan-An <po96106@naver.com>
2025-10-10 17:29:33 +0900

    chore

--- src/main/java/com/knockbook/backend/service/BookService.java
+++ src/main/java/com/knockbook/backend/service/BookService.java
@@ -9,13 +9,11 @@ import com.knockbook.backend.repository.BookReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class BookService {
