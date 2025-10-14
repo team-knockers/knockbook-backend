189dac3411f9c8910ec29a68b51a855e0fb5f471
suhyeon <rainbowjane@naver.com>
2025-10-14 17:51:52 +0900

    Add FeedService#getFeedPosts

--- src/main/java/com/knockbook/backend/service/FeedService.java
+++ src/main/java/com/knockbook/backend/service/FeedService.java
@@ -1,4 +1,21 @@
 package com.knockbook.backend.service;
 
+import com.knockbook.backend.domain.FeedPostsResult;
+import com.knockbook.backend.repository.FeedRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class FeedService {
+    private final FeedRepository feedRepository;
+
+    public FeedPostsResult getFeedPosts(
+            Long userId,
+            String searchKeyword,
+            Long after,
+            int size
+    ) {
+        return feedRepository.findFeedPosts(userId, searchKeyword, after, size);
+    }
 }
