11a9438bfbbf41584f9b804afe58336246067053
suhyeon <rainbowjane@naver.com>
2025-10-15 23:27:54 +0900

    Add FeedControler#getFeedPostComments (GET /feeds/post/{postId}/comments/{userId}) / #getFeedPostWithComments (GET /feeds/post/{postId}/{userId})

--- src/main/java/com/knockbook/backend/controller/FeedController.java
+++ src/main/java/com/knockbook/backend/controller/FeedController.java
@@ -1,9 +1,6 @@
 package com.knockbook.backend.controller;
 
-import com.knockbook.backend.dto.FeedPostDTO;
-import com.knockbook.backend.dto.FeedProfileThumbnailDTO;
-import com.knockbook.backend.dto.GetFeedPostsResponse;
-import com.knockbook.backend.dto.GetFeedProfileResponse;
+import com.knockbook.backend.dto.*;
 import com.knockbook.backend.service.FeedService;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
@@ -59,7 +56,7 @@ public class FeedController {
 
     @PreAuthorize("#userId == authentication.name")
     @GetMapping("/profile/{userId}")
-    public ResponseEntity<GetFeedProfileResponse> getProfilePosts(
+    public ResponseEntity<GetFeedProfileResponse> getFeedProfile(
             @PathVariable("userId") String userId,
             @RequestParam(required = false) String after, // last postId
             @RequestParam @Min(1) int size
@@ -81,6 +78,81 @@ public class FeedController {
                                         .build())
                                 .toList())
                 .nextAfter(result.getNextAfter())
+                .build();
+
+        return ResponseEntity.ok(body);
+    }
+
+    @PreAuthorize("#userId == authentication.name")
+    @GetMapping("/post/{postId}/comments/{userId}")
+    public ResponseEntity<GetFeedCommentsResponse> getFeedPostComments(
+            @PathVariable("userId") String userId,
+            @PathVariable("postId") Long postId
+    ) {
+        final var uid = Long.parseLong(userId);
+        // 서비스 레포지토리 구현 이후 작성 예정
+        final var result = feedService.getFeedPostComments(uid, postId);
+
+        final var feedComments = result.getFeedComments().stream()
+                .map(c -> FeedCommentDTO.builder()
+                        .commentId(c.getCommentId())
+                        .userId(c.getUserId())
+                        .displayName(c.getDisplayName())
+                        .avatarUrl(c.getAvatarUrl())
+                        .body(c.getBody())
+                        .createdAt(DateTimeFormatter.ISO_INSTANT.format(c.getCreatedAt()))
+                        .likedByMe(c.getLikedByMe())
+                        .likesCount(c.getLikesCount())
+                        .build()
+                ).toList();
+
+        final var body = GetFeedCommentsResponse.builder()
+                .feedComments(feedComments)
+                .postId(result.getPostId())
+                .build();
+
+        return ResponseEntity.ok(body);
+    }
+
+    @PreAuthorize("#userId == authentication.name")
+    @GetMapping("/post/{postId}/{userId}")
+    public ResponseEntity<GetFeedResponse> getFeedPostWithComments(
+            @PathVariable("postId") Long postId,
+            @PathVariable("userId") String userId
+    ) {
+        final var uid = Long.parseLong(userId);
+        final var result = feedService.getFeedPostWithComments(uid, postId);
+        final var post = result.getFeedPost();
+
+        final var feedPost = FeedPostDTO.builder()
+                .postId(post.getPostId())
+                .userId(post.getUserId())
+                .displayName(post.getDisplayName())
+                .avatarUrl(post.getAvatarUrl())
+                .content(post.getContent())
+                .images(post.getImages())
+                .likesCount(post.getLikesCount())
+                .commentsCount(post.getCommentsCount())
+                .likedByMe(post.getLikedByMe())
+                .createdAt(DateTimeFormatter.ISO_INSTANT.format(post.getCreatedAt()))
+                .build();
+
+        final var feedComments = result.getFeedComments().stream()
+                .map(c -> FeedCommentDTO.builder()
+                        .commentId(c.getCommentId())
+                        .userId(c.getUserId())
+                        .displayName(c.getDisplayName())
+                        .avatarUrl(c.getAvatarUrl())
+                        .body(c.getBody())
+                        .createdAt(DateTimeFormatter.ISO_INSTANT.format(c.getCreatedAt()))
+                        .likedByMe(c.getLikedByMe())
+                        .likesCount(c.getLikesCount())
+                        .build()
+                ).toList();
+
+        final var body = GetFeedResponse.builder()
+                .feedPost(feedPost)
+                .feedComments(feedComments)
                 .build();
 
         return ResponseEntity.ok(body);
