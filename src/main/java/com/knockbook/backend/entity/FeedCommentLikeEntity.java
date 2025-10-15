102d8fce1161e79694472266af49eab70202f151
suhyeon <rainbowjane@naver.com>
2025-10-15 21:23:46 +0900

    Add Entities: FeedCommentEntity, FeedCommentLikeEntity

--- src/main/java/com/knockbook/backend/entity/FeedCommentLikeEntity.java
+++ src/main/java/com/knockbook/backend/entity/FeedCommentLikeEntity.java
@@ -1,4 +1,24 @@
 package com.knockbook.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "feeds_comment_likes")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class FeedCommentLikeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_like_id", nullable = false)
+    private Long commentLikeId;
+
+    @Column(name = "comment_id", nullable = false)
+    private Long commentId;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
 }
