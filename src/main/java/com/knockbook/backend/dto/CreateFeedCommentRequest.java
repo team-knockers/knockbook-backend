92a1fabcf0740085cdaebb341a2272d207ae350e
suhyeon <rainbowjane@naver.com>
2025-10-16 23:45:30 +0900

    Add RequestDTO: CreateFeedCommentRequest

--- src/main/java/com/knockbook/backend/dto/CreateFeedCommentRequest.java
+++ src/main/java/com/knockbook/backend/dto/CreateFeedCommentRequest.java
@@ -1,4 +1,15 @@
 package com.knockbook.backend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class CreateFeedCommentRequest {
+    @NotBlank
+    @Size(max = 500)
+    private String commentBody;
 }
