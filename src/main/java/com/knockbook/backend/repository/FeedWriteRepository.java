302dbdda296c9799b27fd17381323ecaa25338e2
suhyeon <rainbowjane@naver.com>
2025-10-16 23:46:52 +0900

    Add FeedWriteRepository#insertComment / #incrementPostCommentCounts

--- src/main/java/com/knockbook/backend/repository/FeedWriteRepository.java
+++ src/main/java/com/knockbook/backend/repository/FeedWriteRepository.java
@@ -1,4 +1,13 @@
 package com.knockbook.backend.repository;
 
 public interface FeedWriteRepository {
+    Long insertComment (
+            Long postId,
+            Long userId,
+            String commentBody
+    );
+
+    void incrementPostCommentsCount (
+            Long postId
+    );
 }
--- src/main/java/com/knockbook/backend/repository/FeedWriteRepositoryImpl.java
+++ src/main/java/com/knockbook/backend/repository/FeedWriteRepositoryImpl.java
@@ -1,4 +1,48 @@
 package com.knockbook.backend.repository;
 
-public class FeedWriteRepositoryImpl {
+import com.knockbook.backend.entity.FeedCommentEntity;
+import com.knockbook.backend.entity.QFeedCommentEntity;
+import com.knockbook.backend.entity.QFeedPostEntity;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class FeedWriteRepositoryImpl implements FeedWriteRepository{
+
+    private final JPAQueryFactory query;
+    private final EntityManager em;
+
+    private static final QFeedPostEntity P = QFeedPostEntity.feedPostEntity;
+    private static final QFeedCommentEntity C = QFeedCommentEntity.feedCommentEntity;
+
+    @Override
+    public Long insertComment (
+            Long postId,
+            Long userId,
+            String commentBody
+    ) {
+        final var comment = FeedCommentEntity.builder()
+                .postId(postId)
+                .userId(userId)
+                .body(commentBody)
+                .build();
+
+        em.persist(comment);
+        em.flush();
+
+        return comment.getCommentId();
+    }
+
+    @Override
+    public void incrementPostCommentsCount (
+            Long postId
+    ) {
+        query.update(P)
+                .set(P.commentsCount, P.commentsCount.add(1))
+                .where(P.postId.eq(postId))
+                .execute();
+    }
 }
