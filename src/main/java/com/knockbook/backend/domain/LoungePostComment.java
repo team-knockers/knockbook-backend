32087e75cbf8930c5b33bf4182ddcad90c9fccbe
Yoonhwan-An <po96106@naver.com>
2025-10-17 11:53:51 +0900

    Add Domain for lounge post comments

---
+++ src/main/java/com/knockbook/backend/domain/LoungePostComment.java
@@ -0,0 +1,24 @@
+package com.knockbook.backend.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.Instant;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(toBuilder = true)
+public class LoungePostComment {
+
+    public enum Status { VISIBLE, HIDDEN }
+
+    private final Long id;
+    private final Long postId;
+    private final Long userId;
+    private final String content;
+    private final Status status;
+    private final Instant createdAt;
+    private final Instant updatedAt;
+}
