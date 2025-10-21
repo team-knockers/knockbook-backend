0e8de8da6be011043b1e5c4287f7f5f238a639c3
suhyeon <rainbowjane@naver.com>
2025-10-21 17:33:34 +0900

    Add ProductWishRepository#insertWishlist/ #deleteWishlist

--- src/main/java/com/knockbook/backend/repository/ProductWishRepository.java
+++ src/main/java/com/knockbook/backend/repository/ProductWishRepository.java
@@ -1,4 +1,12 @@
 package com.knockbook.backend.repository;
 
 public interface ProductWishRepository {
+    boolean insertWishlist (
+            Long productId,
+            Long userId
+    );
+    boolean deleteWishlist (
+            Long productId,
+            Long userId
+    );
 }
--- src/main/java/com/knockbook/backend/repository/ProductWishRepositoryImpl.java
+++ src/main/java/com/knockbook/backend/repository/ProductWishRepositoryImpl.java
@@ -1,4 +1,50 @@
 package com.knockbook.backend.repository;
 
-public class ProductWishRepositoryImpl {
+import com.knockbook.backend.entity.ProductWishlistEntity;
+import com.knockbook.backend.entity.QProductWishlistEntity;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ProductWishRepositoryImpl implements ProductWishRepository{
+    private final JPAQueryFactory query;
+    private final EntityManager em;
+
+    private static final QProductWishlistEntity W = QProductWishlistEntity.productWishlistEntity;
+
+    @Override
+    public boolean insertWishlist(
+            Long productId,
+            Long userId
+    ) {
+        final var WishRowExists = query.selectOne()
+                .from(W)
+                .where(W.productId.eq(productId).and(W.userId.eq(userId)))
+                .fetchFirst() != null;
+        if(WishRowExists) { return false; }
+
+        final var wish = ProductWishlistEntity.builder()
+                .productId(productId)
+                .userId(userId)
+                .build();
+
+        em.persist(wish);
+
+        return true;
+    }
+
+    @Override
+    public boolean deleteWishlist(
+            Long productId,
+            Long userId
+    ) {
+        final var affected = query.delete(W)
+                .where(W.productId.eq(productId).and(W.userId.eq(userId)))
+                .execute();
+
+        return affected == 1;
+    }
 }
