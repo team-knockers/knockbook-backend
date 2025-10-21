15b4d57a95fddb96e125729a600613659ffe1e9d
suhyeon <rainbowjane@naver.com>
2025-10-21 18:30:45 +0900

    Add DTO: GetProductWishlist

--- src/main/java/com/knockbook/backend/dto/GetProductWishlist.java
+++ src/main/java/com/knockbook/backend/dto/GetProductWishlist.java
@@ -1,4 +1,15 @@
 package com.knockbook.backend.dto;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class GetProductWishlist {
+    private List<ProductSummaryDTO> products;
 }
