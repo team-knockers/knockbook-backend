26e4ac924a148ae8b3b9cd762311af0a6c828f41
suhyeon <rainbowjane@naver.com>
2025-10-21 18:31:28 +0900

    Add Domain: ProductWishlist

--- src/main/java/com/knockbook/backend/domain/ProductWishlist.java
+++ src/main/java/com/knockbook/backend/domain/ProductWishlist.java
@@ -1,4 +1,15 @@
 package com.knockbook.backend.domain;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class ProductWishlist {
+    private List<ProductSummary> products;
 }
